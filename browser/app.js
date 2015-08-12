b75ffe5516afc79720940f21697731e4098891ab
Mike <mike.sj.lee@gmail.com>
2015-08-11 23:10:53 -0400

    deleted repeat code

--- browser/app.js
+++ browser/app.js
@@ -1,23 +1,13 @@
 var app = angular.module('myApp',['ui.router']);
 
 app.controller('myCtrl', function($scope){
-	
+
 	User.findAll({}).then(function(users){
 		$scope.users = users;
 	});
 
 	$scope.test = "Scope Test"
-	
-	$scope.sendToDB = function (){
-		sequelize.sync().then(function() {
-		  return User.create({
-		    username: $scope.insertText,
-		    birthday: new Date(1980, 6, 20)
-		  });
-		}).then(function(user) {
-		  $scope.users.push(user)
-		});
-	}
+
 
 	$scope.insertInto = function(){
 		User.create({
